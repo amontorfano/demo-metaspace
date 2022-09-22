008219c7f872e7433f5bc496c02261f9820b3ff2
fc0308 <andrea.montorfano@allitude.it>
2022-09-22 16:29:58 +0200

    feat: gitignore

---
+++ src/test/java/it/amontorfano/dsg/demometaspace/DemoMetaspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.amontorfano.dsg.demometaspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMetaspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
